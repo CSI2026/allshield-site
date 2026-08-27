f90789a081ba3a76a6de04dece9eb9dbe72e6d1f
CSI2026 <circlesolutionstx@gmail.com>
2026-08-26 21:12:23 -0500

    Load premium mobile and recruiting polish

--- config.js
+++ config.js
@@ -4,3 +4,13 @@ window.ALLSHIELD_CONFIG = {
   DEMO_FALLBACK: false,
   INTERNAL_EMAIL_DOMAIN: "allshield.internal"
 };
+
+(()=>{
+  const id='allshieldProductionPolishLoader';
+  if(document.getElementById(id))return;
+  const s=document.createElement('script');
+  s.id=id;
+  s.src='./production-polish-2026-08-26.js?v=2026.08.26.009';
+  s.async=false;
+  document.head.appendChild(s);
+})();

1c7c4ae6975ee8a8cf3f988c418dd3d370d61c40
CSI2026 <circlesolutionstx@gmail.com>
2026-08-28 07:31:42 -0500

    Mark production E-Sign release

--- build-info.js
+++ build-info.js
@@ -1,8 +1,8 @@
 window.ALLSHIELD_BUILD_INFO={
   build_number:'B2026.08.23.021',
-  label:'Approved B021 + Automated Onboarding + Recruiting & Leads + Centered Mobile Experience',
+  label:'Approved B021 + Automated Onboarding + Production E-Sign + Recruiting & Leads + Centered Mobile Experience',
   base_build:'B2026.08.23.021',
-  completion_release:'2026.08.28.002',
+  completion_release:'2026.08.28.004',
   channel:'production',
   released_at:'2026-08-28'
 };

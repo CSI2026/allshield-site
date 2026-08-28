8dd346b1a29a7100ea7af5c26d4a368b7098feae
CSI2026 <circlesolutionstx@gmail.com>
2026-08-27 21:07:44 -0500

    Record Recruiting & Leads and mobile symmetry release

--- build-info.js
+++ build-info.js
@@ -1,8 +1,8 @@
 window.ALLSHIELD_BUILD_INFO={
   build_number:'B2026.08.23.021',
-  label:'Approved B021 + Mobile UX + Social Readiness + Professional Careers Video + Audited Runtime Stability',
+  label:'Approved B021 + Automated Onboarding + Recruiting & Leads + Centered Mobile Experience',
   base_build:'B2026.08.23.021',
-  completion_release:'2026.08.27.011',
+  completion_release:'2026.08.28.002',
   channel:'production',
-  released_at:'2026-08-27'
+  released_at:'2026-08-28'
 };
